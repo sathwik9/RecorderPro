1bd9c08c2b6d37640510040aeccb15827c89ddf7
Adam McAvoy <adam@mcavoys.org>
2021-11-29 16:07:31 -0600

    changed button names

--- app/src/main/java/ece/wisc/recorder/MainActivity.java
+++ app/src/main/java/ece/wisc/recorder/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView L1, L2, L3, L4, R1, R2, R3, R4, R3_2, R4_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        L1 = 
     }
 
     public void freePlayButton(View view) {
