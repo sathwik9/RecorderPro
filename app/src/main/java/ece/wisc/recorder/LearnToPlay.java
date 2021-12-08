789e65ab56fa5920f473a47204d714972b47e0c9
Gabrielle Bigalke <gbigalke@wisc.edu>
2021-12-08 16:05:02 -0600

    added song arrays

--- app/src/main/java/ece/wisc/recorder/LearnToPlay.java
+++ app/src/main/java/ece/wisc/recorder/LearnToPlay.java
@@ -8,28 +8,52 @@ import android.view.View;
 
 public class LearnToPlay extends AppCompatActivity {
 
+    private String[] buns;
+    private String[] twinkle;
+    private String[] mary;
+    private String[] shark;
+    private String[] sleep;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_to_play);
+
+        buns = new String[]{"b", "a", "g", "b", "a", "g", "g", "g", "g", "g", "a", "a", "a", "a", "b", "a", "g"};
+        twinkle = new String[]{"c", "c", "g", "g", "a", "a", "g", "f", "f", "e", "e", "d", "d", "c"};
+        mary = new String[]{"b", "a", "g", "a", "b", "b", "b", "a", "a", "a", "b", "b", "b", "b", "a", "g", "a", "b", "b", "b", "a", "a", "b", "a","g"};
+        shark = new String[]{"d", "e", "g", "g", "g","g", "g", "g", "g", "d", "e", "g", "g", "g","g", "g", "g", "g", "d", "e", "g", "g", "g","g", "g", "g", "g", "g", "g", "f"};
+        sleep = new String[]{"g", "a", "b", "a", "g", "a", "g", "a", "b", "a", "b", "g", "g", "a", "b", "a", "g", "a", "g", "a", "b", "a", "b", "g"};
     }
 
     public void hotCrossBuns(View view) {
         Intent intent = new Intent(this, PlayAlong.class);
         startActivity(intent);
-        intent.putExtra("song", "hotCrossBuns");
+        intent.putExtra("song", buns);
     }
 
     public void twinkle(View view) {
         Intent intent = new Intent(this, PlayAlong.class);
         startActivity(intent);
-        intent.putExtra("song", "twinkleTwinkle");
+        intent.putExtra("song", twinkle);
     }
 
     public void maryLamb(View view) {
         Intent intent = new Intent(this, PlayAlong.class);
         startActivity(intent);
-        intent.putExtra("song", "maryHadALittleLamb");
+        intent.putExtra("song", mary);
+    }
+
+    public void babyShark(View view) {
+        Intent intent = new Intent(this, PlayAlong.class);
+        startActivity(intent);
+        intent.putExtra("song", shark);
+    }
+
+    public void gentlySleep(View view) {
+        Intent intent = new Intent(this, PlayAlong.class);
+        startActivity(intent);
+        intent.putExtra("song", sleep);
     }
 
 }
